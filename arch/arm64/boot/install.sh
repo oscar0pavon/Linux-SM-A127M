ce9352ebeaba6fcffacdd2ab40d9fa5efb369aaf
Oscar Pavon <pavon_oscar@outlook.com>
2025-09-30 03:57:50 +0000

    Revert "added framebuffer support for kernel"
    This reverts commit 7e0144acc38f3e06c259c240e31d7739f8c49294.

--- arch/arm64/boot/install.sh
+++ arch/arm64/boot/install.sh
@@ -45,12 +45,16 @@ if [ "$(basename $2)" = "Image.gz" ]; then
 else
 # Normal install
   echo "Installing normal kernel"
-  base=Linux
+  base=vmlinux
 fi
 
-if [ -f $4/$base ]; then
-  mv $4/$base $4/$base.old
+if [ -f $4/$base-$1 ]; then
+  mv $4/$base-$1 $4/$base-$1.old
 fi
-cat $2 > $4/$base
+cat $2 > $4/$base-$1
 
-
+# Install system map file
+if [ -f $4/System.map-$1 ]; then
+  mv $4/System.map-$1 $4/System.map-$1.old
+fi
+cp $3 $4/System.map-$1
